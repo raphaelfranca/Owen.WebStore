8f104d022525c41b9ff9ef4452c41a6129f6a5f8
Raphael França <raphaelfrancabsb@gmail.com>
2016-02-12 18:40:10 -0200

    Inclusão do CrossCutting

---
+++ Owen.WebStore.CrossCutting/DependencyRegister.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.Practices.Unity;
+using Owen.WebStore.ApplicationService;
+using Owen.WebStore.Infraestructure.Persistence;
+using Owen.WebStore.Infraestructure.Persistence.DataContext;
+using Owen.WebStore.Infraestructure.Repositories;
+using Owen.WebStore.SharedKernel;
+using Owen.WebStore.SharedKernel.Events;
+using Owen.WeStore.Domain.Repositories;
+using Owen.WeStore.Domain.Services;
+
+namespace Owen.WebStore.CrossCutting
+{
+    public static class DependencyRegister
+    {
+        /// <summary>
+        /// TransientLifetimeManager - Cada Resolve gera uma nova instância.
+        /// HierarchicalLifetimeManager - Utiliza Singleton
+        /// </summary>
+        /// <param name="container"></param>
+        public static void Register(UnityContainer container)
+        {
+            container.RegisterType<StoreDataContext, StoreDataContext>(new HierarchicalLifetimeManager());
+            container.RegisterType<IUnitOfWork, UnitOfWork>(new HierarchicalLifetimeManager());
+            container.RegisterType<IUserRepository, UserRepository>(new HierarchicalLifetimeManager());
+            container.RegisterType<ICategoryRepository, CategoryRepository>(new HierarchicalLifetimeManager());
+            container.RegisterType<IProductRepository, ProductRepository>(new HierarchicalLifetimeManager());
+            container.RegisterType<IOrderRepository, OrderRepository>(new HierarchicalLifetimeManager());
+
+            container.RegisterType<IUserApplicationService, UserApplicationService>(new HierarchicalLifetimeManager());
+            container.RegisterType<ICategoryApplicationService, CategoryApplicationService>(new HierarchicalLifetimeManager());
+            container.RegisterType<IProductApplicationService, ProductApplicationService>(new HierarchicalLifetimeManager());
+            container.RegisterType<IOrderApplicationService, OrderApplicationService>(new HierarchicalLifetimeManager());
+
+            container.RegisterType<IHandler<DomainNotification>, DomainNotificationHandler>(new HierarchicalLifetimeManager());
+        }
+    }
+}
---
+++ Owen.WebStore.CrossCutting/DomainNotificationHandler.cs
@@ -0,0 +1,41 @@
+﻿using Owen.WebStore.SharedKernel;
+using Owen.WebStore.SharedKernel.Events;
+using System.Collections.Generic;
+
+namespace Owen.WebStore.CrossCutting
+{
+    public class DomainNotificationHandler : IHandler<DomainNotification>
+    {
+        private List<DomainNotification> _notifications;
+
+        public DomainNotificationHandler()
+        {
+            _notifications = new List<DomainNotification>();
+        }
+
+        public void Handle(DomainNotification args)
+        {
+            _notifications.Add(args);
+        }
+
+        public IEnumerable<DomainNotification> Notify()
+        {
+            return GetValue();
+        }
+
+        private List<DomainNotification> GetValue()
+        {
+            return _notifications;
+        }
+
+        public bool HasNotifications()
+        {
+            return GetValue().Count > 0;
+        }
+
+        public void Dispose()
+        {
+            this._notifications = new List<DomainNotification>();
+        }
+    }
+}
